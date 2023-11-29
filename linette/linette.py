eaca97148e96d349e75aba5ff7c1e9fef0f90753
Tim Evans <teva2622@uni.sydney.edu.au>
2023-11-29 12:53:22 +1100

    feat: Initial air-mark of Node and Network classes in linette.py

--- linette/linette.py
+++ linette/linette.py
@@ -1,20 +1,28 @@
-import qutip as qt
 import numpy as np
+import scipy as sp
 from scipy.stats import multivariate_normal
 import tncontract as tn
+from dataclasses import dataclass
+from pydantic import BaseModel, ConfigDict
+from typing import Optional, Any, List
 
-class GaussianRV(object):
-    """
-    Abstract multivariate gaussian
-    """
-    def __init__(self, mean, cov):
-        self.mean = mean
-        self.cov = cov
-        self.rv = multivariate_normal(mean.reshape(-1), cov)
-        super(GaussianRV, self).__init__()
 
-    def __repr__(self):
-        return "Gaussian Random Variable object. Dimension={}".format(self.rv.mean.reshape(-1).shape[0])
+class Node(BaseModel):
+    model_config = ConfigDict(arbitrary_types_allowed=True)
+    data: np.ndarray
 
-    def
 
+class Network(BaseModel):
+    model_config = ConfigDict(arbitrary_types_allowed=True)
+    nodes: List[Node]
+
+
+def main() -> None:
+    """Main function"""
+    mynodes = [Node(data=np.random.randn(2, 2)) for _ in range(5)]
+    mynetwork = Network(nodes=mynodes)
+    print(mynetwork.nodes[0])
+
+
+if __name__ == "__main__":
+    main()
