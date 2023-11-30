5f1cc1808c797fced37ca7bc7d536119a004f45b
Tim Evans <teva2622@uni.sydney.edu.au>
2023-11-30 21:13:42 +1100

    feat: small update to __post_init__ in Node class

--- linette/linette.py
+++ linette/linette.py
@@ -2,26 +2,37 @@ import numpy as np
 import scipy as sp
 from scipy.stats import multivariate_normal
 import tncontract as tn
-from dataclasses import dataclass
+from dataclasses import InitVar
+from pydantic.dataclasses import dataclass
 from pydantic import BaseModel, ConfigDict
-from typing import Optional, Any, List
+from typing import Optional, Any, List, Tuple
 
 
-class Node(BaseModel):
-    model_config = ConfigDict(arbitrary_types_allowed=True)
-    data: np.ndarray
+model_config = ConfigDict(arbitrary_types_allowed=True)
 
 
-class Network(BaseModel):
-    model_config = ConfigDict(arbitrary_types_allowed=True)
+@dataclass(config=model_config)
+class Node:
+    mean: tn.Tensor
+    cov: tn.Tensor
+    shape: Optional[Tuple[int, ...]] = None
+
+    def __post_init__(self):
+        self.shape = self.mean.shape
+
+
+@dataclass(config=model_config, slots=True)
+class Network:
     nodes: List[Node]
 
 
 def main() -> None:
     """Main function"""
-    mynodes = [Node(data=np.random.randn(2, 2)) for _ in range(5)]
-    mynetwork = Network(nodes=mynodes)
-    print(mynetwork.nodes[0])
+
+    # node1 = Node(mean=tn.random_tensor(2, 2), cov=tn.random_tensor(2, 2, 2, 2))
+    # node2 = Node(mean=tn.random_tensor(2, 2), cov=tn.random_tensor(2, 2, 2, 2))
+    # mynetwork = Network(nodes=[node1, node2])
+    # print(mynetwork.nodes[0])
 
 
 if __name__ == "__main__":
